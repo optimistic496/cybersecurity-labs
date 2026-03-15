deb131bcfe289778b127355634dc0d8c9bedb1cd
Mukhtar Busari <158783509+optimistic496@users.noreply.github.com>
2026-03-15 22:37:06 +0100

    Add greeting script to prompt user information
    This script prompts the user for their name, age, department, and matriculation number, then greets them with the provided information.

---
+++ bash-scripts/greeting.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This script greets the user based on the time of day and their name.
+echo " Enter your name: "
+read name
+echo " Please enter your age:"
+read age
+echo " what's your department ? "
+read department
+echo "what is  your matricno?"
+read matricno
+echo "Welcome  $name of $department with $matricno"
