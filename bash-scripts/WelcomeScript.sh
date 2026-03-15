88eb7e7fcd75d4a54107279493f0f4c2162e8555
Mukhtar Busari <158783509+optimistic496@users.noreply.github.com>
2026-03-15 22:09:07 +0100

    Create WelcomeScript.sh for user input greeting
    Add a welcome script that prompts for user name and chapter.

---
+++ bash-scripts/WelcomeScript.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+# this is your second script, in this you prompt 
+# the user for input , place the input in a variable, and 
+# and display the input contents in a string 
+
+echo " what is your name ? "
+read name 
+echo " What chapter are you in on linux basics for hackers ? "
+read chapter
+echo " Welcome $name to Chapter $chapter of Linux Basics for Hackeers "
+
